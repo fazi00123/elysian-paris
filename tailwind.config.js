e63470db02a9949e487553985e60f0cbc13c443f
fazi00123 <162413079+fazi00123@users.noreply.github.com>
2025-09-09 17:44:46 +0500

    complete

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   darkMode: 'class', // Enables dark mode via "class" strategy
+  content: ['./build/**/*.{html,php,css,js}'], // Scans all subfolders within 'build'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
